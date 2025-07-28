ee0e22f9c081c1d00fb0d8ae463fba1e9734c2e3
Saikumarchin2 <mukkamulasaikumar@gmail.com>
2025-07-28 17:03:34 +0530

    new file commited ito final

---
+++ react.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div>
+      <h1>Welcome to E-Commerce</h1>
+      <p>Explore our wide range of products and enjoy great deals!</p>
+    </div>
+  );
+}
+
+export default App;
